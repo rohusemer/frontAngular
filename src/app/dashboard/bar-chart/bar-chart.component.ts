9a6d99dcb8ddecfcc1a93afc01e3840c22c78b93
milenortegam <milenortega_10@hotmail.com>
2021-09-09 13:53:45 -0500

    bar chart

--- src/app/dashboard/bar-chart/bar-chart.component.ts
+++ src/app/dashboard/bar-chart/bar-chart.component.ts
@@ -9,7 +9,12 @@ import * as d3 from 'd3';
 export class BarChartComponent implements OnInit {
 
   constructor() { }
-
+  private data = [
+    {"cuentaid": 1, "monto": 200, "tipo": "efectivo", "fecha": "2021-09-09" },
+    {"cuentaid": 2, "monto": 3000, "tipo": "ahorro", "fecha": "2013-09-09"},
+    {"cuentaid": 3, "monto": 45, "tipo": "tDebito", "fecha": "2018-09-09"},
+    {"cuentaid": 4, "monto": 700, "tipo": "tCredito", "fecha": "2017-09-09"}
+  ];
   private svg;
   private margin = 50;
   private width = 750 - (this.margin * 2);
@@ -24,11 +29,11 @@ export class BarChartComponent implements OnInit {
     .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
   }
 
-  private drawBars(data: any): void {
+  private drawBars(data: any[]): void {
     // Create the X-axis band scale
     const x = d3.scaleBand()
     .range([0, this.width])
-    .domain(data.map(d => d.nombre))
+    .domain(data.map(d => d.tipo))
     .padding(0.2);
 
     // Draw the X-axis on the DOM
@@ -41,7 +46,7 @@ export class BarChartComponent implements OnInit {
 
     // Create the Y-axis band scale
     const y = d3.scaleLinear()
-    .domain([0, 5])
+    .domain([0, 4000])
     .range([this.height, 0]);
 
     // Draw the Y-axis on the DOM
@@ -53,15 +58,16 @@ export class BarChartComponent implements OnInit {
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", d => x(d.nombre))
-    .attr("y", d => y(d.id))
+    .attr("x", d => x(d.tipo))
+    .attr("y", d => y(d.monto))
     .attr("width", x.bandwidth())
-    .attr("height", (d) => this.height - y(d.id))
+    .attr("height", (d) => this.height - y(d.monto))
     .attr("fill", "#d04a35");
   }
   ngOnInit(): void {
     this.createSvg();
-    d3.json('http://localhost:4000/api/cliente').then(data => this.drawBars(data));
+    this.drawBars(this.data);
+    //d3.json('http://localhost:4000/api/cliente').then(data => this.drawBars(data));
   }
 
 }
